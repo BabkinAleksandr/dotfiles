8fa68c7450eeb480772557c1af740f04a7376322
Aleksandr Babkin <aleksanderbabkin123@gmailc.com>
2024-05-02 21:20:33 +0100

    nvim: up bufferline config use tabs again, simplify layout

--- nvim/.config/nvim/lua/aleks/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/aleks/plugins/bufferline.lua
@@ -4,13 +4,19 @@ return {
     version = "*",
     opts = {
         options = {
-            -- mode = "tabs",
-            move = "buffers",
+            mode = "tabs",
+            -- move = "buffers",
             diagnostics = "nvim_lsp",
             diagnostics_indicator = function(count, level)
                 local icon = level:match("error") and " " or " "
                 return " " .. icon .. count
             end,
+            tab_size = 40,
+            max_name_length = 40,
+            always_show_bufferline = true, -- change to false to entirely hide bufferline
+            show_close_icon = false,
+            show_buffer_close_icons = false,
+            show_tab_indicators = false,
             close_command = "Bdelete! %d",
             right_mouse_command = "Bdelete! %d",
         },
