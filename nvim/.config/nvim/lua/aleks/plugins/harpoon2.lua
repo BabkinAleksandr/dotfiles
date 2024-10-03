ccad37a37f4873edc7769bcde1273d0e80421eb8
Aleksandr Babkin <aleksanderbabkin123@gmail.com>
2024-10-03 13:36:12 +0100

    nvim: fix harpoon and lsp configs

--- nvim/.config/nvim/lua/aleks/plugins/harpoon2.lua
+++ nvim/.config/nvim/lua/aleks/plugins/harpoon2.lua
@@ -10,30 +10,30 @@ return {
 
         vim.keymap.set("n", "<leader>a", function()
             harpoon:list():add()
-        end)
+        end, { desc = "Harpoon: Add file" })
         vim.keymap.set("n", "<C-e>", function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
         end)
 
         vim.keymap.set("n", "<leader>1", function()
             harpoon:list():select(1)
-        end)
+        end, { desc = "Harpoon: Go To 1" })
         vim.keymap.set("n", "<leader>2", function()
             harpoon:list():select(2)
-        end)
+        end, { desc = "Harpoon: Go To 2" })
         vim.keymap.set("n", "<leader>3", function()
             harpoon:list():select(3)
-        end)
+        end, { desc = "Harpoon: Go To 3" })
         vim.keymap.set("n", "<leader>4", function()
             harpoon:list():select(4)
-        end)
+        end, { desc = "Harpoon: Go To 4" })
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<C-p>", function()
             harpoon:list():prev()
-        end)
+        end, { desc = "Harpoon: Prev" })
         vim.keymap.set("n", "<C-n>", function()
             harpoon:list():next()
-        end)
+        end, { desc = "Harpoon: Next" })
     end,
 }
