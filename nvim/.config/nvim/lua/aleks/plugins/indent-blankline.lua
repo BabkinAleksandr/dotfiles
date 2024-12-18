3d6a9becaf712a2a91b25162694f07d8c27f1095
Aleksandr Babkin <aleksanderbabkin123@gmail.com>
2024-12-18 09:37:41 +0000

    nvim: plugins

--- nvim/.config/nvim/lua/aleks/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/aleks/plugins/indent-blankline.lua
@@ -2,16 +2,17 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPre", "BufNewFile" },
     main = "ibl",
+    enabled = false,
     config = function()
-        -- vim.opt.list = true
-        -- vim.opt.listchars:append("space:·")
-        --
-        -- local ibl = require("ibl")
-        -- ibl.setup({
-        --     scope = {
-        --         enabled = false,
-        --     },
-        --     indent = { char = "┊" },
-        -- })
+        vim.opt.list = true
+        vim.opt.listchars:append("space:·")
+
+        local ibl = require("ibl")
+        ibl.setup({
+            scope = {
+                enabled = false,
+            },
+            indent = { char = "┊" },
+        })
     end,
 }
